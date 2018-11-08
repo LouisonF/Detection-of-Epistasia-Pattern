b3ba4b367b0ae663da051f27feaf75dae1eaed8d
Louison Fresnais <fresnaislouison@gmail.com>
2018-11-08 23:39:55 +0100

    Data naive simulation script update

--- data_simulation/parameter_parsing.py
+++ data_simulation/parameter_parsing.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python3
 #-*-coding: utf-8-*-
 # Louison Fresnais M2BB
+# François Courtin M2BB
 
 #####################################################################
 #   Cette fonction permet le parsing des paramètres du script		#
@@ -16,7 +17,8 @@ def argument_parsing():
         epilog="""The objective of this script is to create naively simulated data for an
             epistasia pattern detection algorithm.
             The output of this script is:  
-                - a nomber of files (defined by user) with simulated TP/FP/FN
+                - a number of simulated genotype data
+                - a number of simulated phenotype data
             
             Input parameters are the following: 
             -1: Number of files to generate per run
@@ -29,7 +31,7 @@ def argument_parsing():
             Contact: fresnaislouison@gmail.com""")
 
     parser.add_argument('-nf','--file', type=int, help='enter the number of file you want to generate in one run')
-    parser.add_argument('-p','--prefix', type=str, help='enter a disctinctive prefix that will be applied to each result file for this run')
+    parser.add_argument('-pf','--prefix', type=str, help='enter a disctinctive prefix that will be applied to each result file for this run')
     parser.add_argument('-nv','--variable', type=int, help='enter the number of variables to simulate')
     parser.add_argument('-nc','--case', type=int, help='enter the number of case to simulate')
     parser.add_argument('-no','--control', type=int, help='enter the number of controls to simulate')
--- data_simulation/simu_naive.py
+++ data_simulation/simu_naive.py
@@ -2,10 +2,14 @@
 #-*-coding: utf-8-*-
 #pas de shebang pour pouvoir lancer dans l'environnement conda, mettre python devant le ./file_to_launch
 # Louison Fresnais M2BB
+# François Courtin M2BB
 
-
+# This script requires numpy (pip install numpy)
 import sys
+import numpy as np
+import re
 
+from parameter_parsing import *
 
 
 args = vars(argument_parsing())
@@ -25,12 +29,12 @@ except TypeError:
 	number_file = int(input("enter the number of file you want to generate in one run"))
 
 ###Prefix value reading
-try:
-	prefix = str(args.get("prefix"))
-	print("Prefix value: ",prefix)
+prefix = str(args.get('prefix'))
+print("Prefix value: ",prefix)
+if (prefix == 'None'):
 
-except TypeError:
-	prefix = int(input("enter a disctinctive prefix that will be applied to each result file for this run"))
+	prefix = str(input("enter a disctinctive prefix that will be applied to each result file for this run"))
+
 
 ###Number of variables reading
 try:
@@ -62,3 +66,31 @@ except TypeError:
 
 
 
+
+for i in range(1,number_file+1):
+	#file names creation
+	geno_file_name = str(prefix+"genotypes"+str(i)+".txt")
+	pheno_file_name = str(prefix+"phenotypes"+str(i)+".txt")
+	#SNPs IDs matrix creation
+	matrix_genotype_ID = []
+	for x in range(1,number_variable+1):
+		x = str(x)
+		ID = "N"+x
+		matrix_genotype_ID.append(ID)
+
+	#Convert the ID list into an array
+	matrix_genotype_ID = np.asarray(matrix_genotype_ID)
+	#Matrix random creation
+	matrix_case_geno = np.random.randint(low=0,high=3, size=(number_case, number_variable),dtype="int")
+	matrix_control_geno = np.random.randint(low=0,high=3, size=(number_control, number_variable))
+	matrix_case_pheno = np.random.randint(low=0,high=2, size=(number_case, 1))
+	matrix_control_pheno = np.random.randint(low=0,high=2, size=(number_control, 1))
+
+	#vstack will concatenate by rows case and control array
+	matrix_final_geno = np.vstack((matrix_case_geno,matrix_control_geno))
+	#save the simulated genotype data file
+	np.savetxt(geno_file_name,np.r_[[matrix_genotype_ID], matrix_final_geno], fmt='%s', delimiter=',')
+
+	matrix_final_pheno = np.vstack((matrix_case_pheno,matrix_control_pheno))
+	#save the simulated phenotype data file
+	np.savetxt(pheno_file_name,np.r_[matrix_final_pheno], fmt='%s', delimiter=',')
