68b04d9aa498728650129c39d1da5df953e9e72a
Louison Fresnais <fresnaislouison@gmail.com>
2018-11-09 16:12:19 +0100

    add TODO

---
+++ epistasia_detection/datainput.cpp
@@ -0,0 +1,24 @@
+/*
+ * datainput.cpp
+ *
+ *  Created on: 9 nov. 2018
+ *      Author: louison
+ */
+
+#include "datainput.h"
+
+namespace std {
+//Constructeur
+data_input::data_input() {
+	// TODO Auto-generated constructor stub
+
+}
+//Destructeur
+data_input::~data_input() {
+	// TODO Auto-generated destructor stub
+}
+
+//Debut des méthodes
+//***********************************************************************************************************
+
+} /* namespace std */
---
+++ epistasia_detection/datainput.h
@@ -0,0 +1,23 @@
+/*
+ * datainput.h
+ *
+ *  Created on: 9 nov. 2018
+ *      Author: louison
+ */
+
+#ifndef DATAINPUT_H_
+#define DATAINPUT_H_
+
+namespace std {
+#include<boost>
+
+
+class data_input {
+public:
+	data_input();
+	virtual ~data_input();
+};
+
+} /* namespace std */
+
+#endif /* DATAINPUT_H_ */
