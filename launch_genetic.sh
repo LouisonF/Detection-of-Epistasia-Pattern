e47a9d3711a2b0a88cda0f189a6dd02073d1fd11
Courtin <francois.courtin.l@gmail.com>
2019-01-23 21:40:36 +0100

    update parameters genet and launcher genet

--- launch_genetic.sh
+++ launch_genetic.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 #run with ./launch_genetic.sh   name_of_the_output_files   geno_path   pheno_path
 
-#ex : ./launch_genetic.sh res Utils/data_simulation/test_perturb_all_files/test_perturbgenotypes1.txt Utils/data_simulation/test_perturb_all_files/test_perturbphenotypes1.txt 
+#ex : ./launch_genetic.sh res /home/courtin/Documents/M2/ProjetC/Simu_naive_2snp_0.25 /home/courtin/Documents/M2/ProjetC/Simu_naive_2snp_0.25_pheno
 
-for i in {1..20}
+_debut=$(date +%s)
+
+for j in {1..100}
 do
-  ./Genetic/Debug/Genetic "$1_$i" "$2" "$3"
+  for i in {1..20}
+  do
+    debut=$(date +%s)
+    echo "FILE NUMBER : $j"
+    echo "ITERATION NUMBER : $j"
+    ./Genetic/Debug/Genetic "$1_$i" "$2/Naif_$j""_Genotype.txt" "$3/Naif_$j""_Phenotype.txt"
+    fin=$(date +%s)
+
+    duree=$(( $fin - $debut ))
+    echo "$duree" >> tps.txt
+  done
 done
+_fin=$(date +%s)
+
+duree=$(( $_fin - $_debut ))
+echo "$duree" >> tps.txt
