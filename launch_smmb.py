93486d8e848c34e8a4d8ea001f91c418547ca531
Louison Fresnais <fresnaislouison@gmail.com>
2019-02-13 18:34:21 +0100

    Update launch_smmb

--- launch_smmb.py
+++ launch_smmb.py
@@ -14,6 +14,10 @@ from subprocess import Popen, PIPE
 data_path = sys.argv[1] #Give the data directory
 dataset = sys.argv[2] #nom du jeu de données
 param_path = sys.argv[3] #path to the parameters file
+number_snps = sys.argv[4] # the number of snps in the dataset
+size_epistasia = sys.argv[5] # the size of the epistasia pattern
+
+nbr_runs = 2 #change this value in order to modifiy the number of runs
 try:
     os.makedirs("SMMB-ACO_results/"+dataset)
     os.makedirs("SMMB-ACO_eval_results")
@@ -43,20 +47,20 @@ for geno in pipe_geno.stdout:
     except FileExistsError:
         print("Directory already exist")
 
-    for i in range(1,2):
+    for i in range(1,nbr_runs):
         pheno_str = str(pheno_list[j])
         print(pheno_str)
         exec_start_time = int(round(time.time() * 1000))
-        os.system("./SMMB-ACO/SMMB-ACO "+geno_dir+"/res_"+geno_temp+"_"+str(i)+" "+data_path+"/"+geno_temp+".txt "+data_path+"/"+pheno_str+ " " +param_path)
+        os.system("./SMMB-ACO/smmb_aco "+geno_dir+"/res_"+geno_temp+"_"+str(i)+" "+data_path+"/"+geno_temp+".txt "+data_path+"/"+pheno_str+ " " +param_path)
         exec_end_time = str(float(int(round(time.time() * 1000)) - exec_start_time)/1000)
         file.write(exec_end_time+"s\n")
 
     j+=1
 end_time = str(float((int(round(time.time() * 1000)) - start_time))/1000/60)
 print(end_time)
-file.write(end_time+"min")
+file.close()
 
 #LAUNCH EVAL SCRIPT
 #run : ./eval_simu.py input_directory_path output_directory_path n_runs nb_snp len_pattern
 #The input directory path must be <Model_results_directory>/<Jeu_donnees_x_directory> with a directory /<fichier_simulé_x> inside containing the n itération for that file.
-os.system("./eval/eval_simu.py " "/home/louison/Documents/FAC/M2/c++_project/detection-of-epistasia-pattern/SMMB-ACO_results/"+dataset+ " /home/louison/Documents/FAC/M2/c++_project/detection-of-epistasia-pattern/SMMB-ACO_eval_results 20 100 2")
+os.system("./eval/eval_simu.py " "/comptes/E146938Q/c++_project/rendu_smmb_aco/SMMB-ACO_results/"+dataset+ " /comptes/E146938Q/c++_project/rendu_smmb_aco/SMMB-ACO_eval_results "+str(nbr_runs)+" "+str(number_snps)+" "+str(size_epistasia))
